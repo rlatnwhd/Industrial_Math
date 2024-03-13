2f2afb4f36777a1b11382d1ecff8b48967002543
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-03-13 15:28:18 +0900

    2024_0313

---
+++ 0313/ch04_125.py
@@ -0,0 +1,14 @@
+'''
+작성일 : 2024년 3월 13일
+작성자 : 컴퓨터공학부 202395007 김수종
+설명 : 수학 연산을 해주는 math 함수 활용
+'''
+
+import math
+
+print("3의 제곱근 =",math.sqrt(3)) # sqrt (제곱근)
+print("10의 지수함수 =",math.exp(10)) # exp (지수함수)
+print("1의 자연로그값 =",math.log(1)) # log (로그)
+print("파이의 코사인 값 =",math.cos(math.pi * 0)) # cos (코사인)
+print("파이의 사인값 =",math.sin(math.pi * 3 / 2)) # sin (사인)
+print("파이의 탄젠트값 =",math.tan(math.pi / 4)) # tan (탄젠트)
